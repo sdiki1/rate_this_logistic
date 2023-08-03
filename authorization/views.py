004bfe751dfb93f30cad72260eb5556c82fee4fc
sdiki1 <volodymyrbuharin@yandex.ru>
2023-08-04 01:57:28 +0300

    added auth

--- authorization/views.py
+++ authorization/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
+
+def auth(request):
+    if request.method != "POST":
+        if request.user.is_authenticated:
+            return HttpResponse("Вы уже авторизованы!")
+        return render(request, "Auth_page.html")
+
+    User = authenticate(username=request.POST.get("username"), password=request.POST.get("password"))
+    if User is None:
+        return render(request, "Auth_page_err.html")
+    return HttpResponseRedirect('/')
---
+++ main/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+import main.views
+
+
+urlpatterns = [
+
+]
